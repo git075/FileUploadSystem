f7cf6d1fd1dc28cc4bde7234b0244487dea5b560
Anurag Ghosliya <72004450+git075@users.noreply.github.com>
2025-01-23 17:09:57 +0530

    Update QRCodeController.java

--- src/main/java/com/printshop/controller/QRCodeController.java
+++ src/main/java/com/printshop/controller/QRCodeController.java
@@ -17,10 +17,8 @@ public class QRCodeController {
     private IPAddressUtil ipAddress;
 
     @GetMapping("/{shopId}")
-    public ResponseEntity<byte[]> getQRCode(@PathVariable String shopId) throws Exception { //Changed shopid to string from long.
-       // String shopUrl = "http://localhost:8080/shop/" + shopId;
+    public ResponseEntity<byte[]> getQRCode(@PathVariable String shopId) throws Exception { 
     	String hostAddress = ipAddress.getIpAddress();
-       // String shopUrl = "http://172.21.88.60:8080/api/files/" + shopId + "/upload-form";//Because if there were localhost, then only if i scan the qr with this pc then it would work because scanning through phone will understand localhost to itself. Also the server is running of this pc.
         String shopUrl = "http://" + hostAddress + ":8080/api/files/" + shopId + "/upload-form";
         byte[] qrCode = qrCodeGeneratorService.generateQRCode(shopUrl, 200, 200);
         
